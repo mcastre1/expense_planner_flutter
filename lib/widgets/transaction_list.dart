d6539be8fd2a9a14b2ea911c9b230cb13130950b
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2021-11-07 09:35:17 -0700

    Tile in Card

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -19,8 +19,15 @@ class TransactionList extends StatelessWidget {
                   'No Transactions added yet!',
                   style: Theme.of(context).textTheme.headline6,
                 ),
-                SizedBox(height: 20,),
-                Container(height: 200, child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
               ],
             )
           :
@@ -28,47 +35,24 @@ class TransactionList extends StatelessWidget {
           ListView(
               children: transactions.map((tx) {
                 return Card(
-                    child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text('\$${tx.amount}'),
+                          )),
                     ),
-                    Column(
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.black,
-                          ),
-                        ),
-                        Text(
-                          DateFormat().format(tx.date),
-                          style: TextStyle(color: Colors.grey, fontSize: 15),
-                        ),
-                      ],
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    )
-                  ],
-                ));
+                    title: Text(
+                      tx.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(DateFormat.yMMMMd().format(tx.date)),
+                  ),
+                );
               }).toList(),
             ),
     );
